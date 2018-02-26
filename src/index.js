b19c6404e124eebb965d8bdfa387d0f0b161eef4
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-02-26 19:04:46 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var nulls = 0;
+    var i = 1;
+    //var logfm = Math.log(5) / Math.log(10);
+    for (var j = 5; j <= number; j*=5)
+    {
+        var rest = number % j;
+        number = number - rest;
+        nulls += (number / j);
+        i++;
+        }
+    return nulls;
+    //return logfm;
 }
